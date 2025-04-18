f226c0e8637090ae92023dd0f15b90170db852a5
desmondflexy <ikilodesmond@gmail.com>
2025-04-18 01:09:19 +0100

    first commit

---
+++ index.mjs
@@ -0,0 +1,22 @@
+import express from 'express';
+import { createServer } from 'http';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { connectWebSocket } from './socket.mjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const server = createServer(app);
+
+app.get("/", (_, res) => {
+    res.sendFile(join(__dirname, 'client.html'));
+});
+
+connectWebSocket(server);
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
